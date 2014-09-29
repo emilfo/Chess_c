e9812933b6c335abf84fa6b1fada558d296c5052
skalkanskje <skalkanskje@gmail.com>
2014-09-30 01:52:18 +0200

    wrote clearforsearch mothod that resets the board

--- search.c
+++ search.c
@@ -17,7 +17,27 @@ static int IsRepetition(const S_BOARD *pos) {
 	return FALSE;
 }
 
-static void ClearForSearch(S_BOARD *pos, S_SEARCHINFO *info) {//TODO
+static void ClearForSearch(S_BOARD *pos, S_SEARCHINFO *info) {
+	int i, j;
+
+	for (i = 0; i < 13; i++) {
+		for (j = 0; j < BRD_SQ_NUM; j++) {
+			pos->searchHistory[i][j] = 0;
+		}
+	}
+
+	for (i = 0; i < 2; i++) {
+		for (j = 0; j < MAXDEPTH; j++) {
+			pos->searchKillers[i][j] = 0;
+		}
+	}
+
+	ClearPvTable(pos->PvTable);
+	pos->ply = 0;
+
+	info->starttime = GetTimeMs();
+	info->stopped = 0;
+	info->nodes = 0;
 }
 
 static int AlphaBeta(int alpha, int beta, int depth, S_BOARD *pos,
